ad1c7a21cb19f8637b7828003ca7cd1183a375bf
aras arkan <arasarkan9@gmail.com>
2026-09-15 00:35:15 +0300

    breaking up tanh

--- week2/manualBackprop.py
+++ week2/manualBackprop.py
@@ -1,5 +1,6 @@
 from graphviz import Digraph
 import math
+import os
 
 class Value:
 
@@ -15,6 +16,7 @@ class Value:
         return f"Value(data= {self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
         def _backward():
             self.grad += 1.0 * out.grad
@@ -23,20 +25,51 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), "*")
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
+        out._backward = _backward 
+        return out
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
         out._backward = _backward
         return out
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
 
     def tanh(self):
         x = self.data
-        t =(math.exp(2*x) -1) / (math.exp(2*x) + 1)
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(t, (self, ), 'tanh')
 
         def _backward():
             self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
 
         out._backward = _backward
         return out
@@ -49,7 +82,7 @@ class Value:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
-            topo.append(v)
+                topo.append(v)
         build_topo(self)
 
         self.grad = 1.0
@@ -109,8 +142,10 @@ x2w2 = x2*w2; x2w2.label = 'x2w2'
 x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1 + x2w2'
 n = x1w1x2w2 + b; n.label = 'n'
 
-o = n.tanh()
+e = (2*n).exp()
+o =(e-1) / (e+1)
 o.backward()
 
 dot = draw_dot(o); o.label = 'o'
-dot.render('graph', view=False, cleanup=True)   
+BURASI = os.path.dirname(os.path.abspath(__file__))
+dot.render(os.path.join(BURASI, 'graph'), view=False, cleanup=True)   
